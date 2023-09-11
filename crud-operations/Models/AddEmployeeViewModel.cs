40334ad4ba2508676500473d0918b2f1146ac557
limzhengyang <>
2023-09-11 16:25:27 +0800

    add validation while adding employees

--- crud-operations/Models/AddEmployeeViewModel.cs
+++ crud-operations/Models/AddEmployeeViewModel.cs
@@ -1,14 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace crud_operations.Models;
 
 public class AddEmployeeViewModel
 {
+    [Required]
+    [Display(Name="Your Name")]
     public string Name { get; set; }
     
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
     
+    [Required]
     public long Salary { get; set; }
     
+    [Required]
     public DateTime DateOfBirth { get; set; }
     
+    [Required]
     public string Department { get; set; }
 }
